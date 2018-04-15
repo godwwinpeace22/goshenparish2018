7f619f27e3d60bb5e350425202d0c37850f79cda
godwin gabriel <godwinpeace22@gmail.com>
2018-04-16 00:02:50 +0100

    vital delete

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const Sermon = require('../models/sermon');
 const Event = require('../models/event');
 const Image = require('../models/image');
